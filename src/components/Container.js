017e5d37f478a3c82fb6c3ada48d73ba207d021e
alshimaa-mhmd <alshimaa717@gmail.com>
2024-10-05 01:33:37 +0300

    edit calculations and resolution

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,15 +8,53 @@ function Container(){
     const [amount, setAmount] = useState('');
     const[term, setTerm] = useState('');
     const[rate, setRate] = useState('');
-    const [monthlyPayment, setMonthlyPayment] = useState(0);
-    const [empty, setEmpty] = useState(true);
-    const monthly_interest_rate = rate/(100 * 12);
     const [selected, setSelected] = useState("");
-    const total_payments = term * 12;
-    const totalRepayment = (Math.round( (monthlyPayment * total_payments) * 100) / 100).toLocaleString();
+    const [amountError, setAmountError] = useState(false);
+    const [termError, setTermError] = useState(false);
+    const [rateError, setRateError] = useState(false);
+    const [selectedError, setSelectedError] = useState(false);
 
-    const monthlyInterest =  (Math.round((amount * monthly_interest_rate) *100) /100) ;
-    const totalInterest = ( monthlyInterest * term * 12).toLocaleString();    
+    const [monthlyPayment, setMonthlyPayment] = useState(0);
+    const [monthlyInterest, setMonthlyInterest] = useState(0);
+    const [totalInterest, setTotalInterest] = useState(0);
+    const [totalRepayment, setTotalRepayment] = useState(0);
+
+    const [empty, setEmpty] = useState(true);
+
+    const monthly_interest_rate = rate/(100 * 12);
+
+    const total_payments = term * 12;
+
+    function calculateMonthlyPayment (){
+
+        if(!amount) {
+         setAmountError(true);
+        }else{setAmountError(false)}
+        if(!rate) {
+         setRateError(true);
+        }else{setRateError(false)}
+        if(!term) {
+         setTermError(true);
+        }else{setTermError(false)}
+        if(!selected) {
+         setSelectedError(true);
+}else{setSelectedError(false)}
+        if(!amount || !rate || !term || !selected) return ;
+         
+         setEmpty(false);
+
+         setMonthlyPayment((monthlyPayment) => monthlyPayment = (amount * monthly_interest_rate * (1 + monthly_interest_rate ) ** total_payments ) / (((1 + monthly_interest_rate)** total_payments) -1 ) );
+         setMonthlyInterest((x) => x =(Math.round((amount * monthly_interest_rate) *100) /100)) ;
+
+         setTotalRepayment(x => x = (Math.round( (((amount * monthly_interest_rate * (1 + monthly_interest_rate ) ** total_payments ) / (((1 + monthly_interest_rate)** total_payments) -1 )) * total_payments) * 100) / 100).toLocaleString());
+         setTotalInterest(x => x =( (Math.round((amount * monthly_interest_rate) *100) /100) * term * 12).toLocaleString()); 
+
+         setAmountError(false);
+         setRateError(false);
+         setTermError(false);
+         setSelectedError(false);
+     }
+   
     return(
             <MortgageContext.Provider value={{
                 amount,
@@ -27,6 +65,7 @@ function Container(){
                 setRate,
                 monthlyPayment,
                 totalRepayment,
+                setTotalRepayment,
                 setMonthlyPayment,
                 monthly_interest_rate,
                 total_payments,
@@ -35,6 +74,13 @@ function Container(){
                 setSelected,
                 monthlyInterest,
                 totalInterest,
+                setTotalInterest,
+                calculateMonthlyPayment,
+                amountError,
+                rateError,
+                termError,
+                selectedError,
+                setMonthlyInterest,
             }}>
                 <div className="xl:flex h-[600px]">
                     <Calculator />
--- src/components/Result.js
+++ src/components/Result.js
@@ -3,10 +3,11 @@ import { useContext } from "react";
 function Result(){
     const {monthlyPayment, totalRepayment, selected, monthlyInterest,
         totalInterest,} = useContext(MortgageContext);
+
     const roundedPayment = Math.round(monthlyPayment * 100) / 100;
 
     return(
-    <div className="bg-[#133040] p-16 xl:p-8 mx-[-32px] xl:w-[50%] xl:mt-0 xl:h-full xl:rounded-r-3xl xl:rounded-bl-[80px] xl:-ml-10" >
+    <div className="bg-[#133040] p-6 xl:p-8 xl:mx-[-32px] xl:w-[50%] xl:mt-0 xl:h-full xl:rounded-r-3xl xl:rounded-bl-[80px] xl:-ml-10" >
         <h1 className="text-slate-100 font-bold text-xl">Your results</h1>
         <br />
         <p className="leading-6 text-slate-400 font-semibold">
@@ -15,14 +16,14 @@ function Result(){
         <br />
 
         <div className="bg-[#0e2431] border-t-4 border-[#d9da31] rounded-md p-8 xl:h-[60%]">
-            <h3 className="text-slate-400 font-semibold mb-8">Your monthly repayments</h3>
-            <span className="text-[#d9da31] text-6xl font-semibold leading-8">
+            <h3 className="text-slate-400 font-semibold mb-8 text-[14px] sm:text-[25px]">Your monthly repayments</h3>
+            <span className="text-[#d9da31] text-[30px] sm:text-6xl font-semibold leading-8">
                 €{selected === "repayments" && roundedPayment}
                 {selected === "interest" && monthlyInterest}
                 </span>
-            <hr className="border-slate-500 my-10"/>
-            <h3 className="text-slate-400 mb-4 font-semibold">Total you'll repay over the term</h3>
-            <span className="text-4xl text-slate-100 leading-8">
+            <hr className="border-slate-500 xl:my-8 my-6"/>
+            <h3 className="text-slate-400 text-[14px] sm:text-[25px] mb-4 font-semibold">Total you'll repay over the term</h3>
+            <span className="text-[25px] sm:text-4xl text-slate-100 leading-8">
                 €{selected === "interest" && totalInterest}
                 {selected === "repayments" && totalRepayment}
                 </span>
